8238328d15d94dbf680e08afa20c6a5572cae9ce
DESKTOP-QMGOQCG\CSI21-Amorlez <csi21-amorlez@colegioaltair.net>
2023-01-11 14:26:54 +0100

    Formateo de codigo

--- EjercicioLinQ/Program.cs
+++ EjercicioLinQ/Program.cs
@@ -6,35 +6,38 @@ List<int> listEnteros = new List<int>();
 for (int i = 0; i < 100; i++)
     listEnteros.Add(i);
 
+
+#region Query syntax
 Console.WriteLine("Mostrando con Query syntax");
-#region Primera forma 
 var divisibleEntre7 =
     from entre in listEnteros
     where (entre % 7) == 0
     select entre;
 
-foreach (int i in divisibleEntre7)
-    Console.WriteLine(i);
+
 Console.WriteLine();
 var esPar =
     from par in divisibleEntre7
     where (par % 2) == 0
     select par;
-Console.WriteLine();
+
 foreach (int i in esPar)
     Console.WriteLine(i);
-Console.WriteLine();
+
 var esInPar =
     from inpar in divisibleEntre7
     where (inpar % 2) != 0
     select inpar;
 
-foreach (int i in esInPar)
-    Console.WriteLine(i);
+foreach (int i in esPar) Console.WriteLine(i);
+Console.WriteLine();
+foreach (int i in divisibleEntre7) Console.WriteLine(i);
+Console.WriteLine();
+foreach (int i in esInPar) Console.WriteLine(i);
 
 #endregion
 
-#region Segunda forma 
+#region Method syntax
 Console.WriteLine("Mostrando con Method syntax");
 var divisibleEntreSiete = listEnteros.Where(x => x % 7 == 0);
 
