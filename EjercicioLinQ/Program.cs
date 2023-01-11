40fce8169339343bb8e301af2f93364baccd188d
DESKTOP-QMGOQCG\CSI21-Amorlez <csi21-amorlez@colegioaltair.net>
2023-01-11 14:08:04 +0100

    Agregar archivos de proyecto.

---
+++ EjercicioLinQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+List<int> listEnteros = new List<int>();
+
+for (int i = 0; i < 100; i++)
+    listEnteros.Add(i);
+
+var divisibleEntre7 =
+    from entre in listEnteros
+    where (entre % 7) == 0
+    select entre;
+
+foreach(int i in divisibleEntre7)
+    Console.WriteLine(i);
