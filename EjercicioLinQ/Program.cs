2327d9c60174e0bfac4ed15f417e5c5ced23a806
DESKTOP-QMGOQCG\CSI21-Amorlez <csi21-amorlez@colegioaltair.net>
2023-01-11 14:22:58 +0100

    Cambio en la presentacion de los resultados

--- EjercicioLinQ/Program.cs
+++ EjercicioLinQ/Program.cs
@@ -6,7 +6,7 @@ List<int> listEnteros = new List<int>();
 for (int i = 0; i < 100; i++)
     listEnteros.Add(i);
 
-Console.WriteLine("Mostrando de la primera forma");
+Console.WriteLine("Mostrando con Query syntax");
 #region Primera forma 
 var divisibleEntre7 =
     from entre in listEnteros
@@ -16,12 +16,12 @@ var divisibleEntre7 =
 foreach (int i in divisibleEntre7)
     Console.WriteLine(i);
 Console.WriteLine();
-var esPar = 
+var esPar =
     from par in divisibleEntre7
     where (par % 2) == 0
     select par;
 Console.WriteLine();
-foreach(int i in esPar)
+foreach (int i in esPar)
     Console.WriteLine(i);
 Console.WriteLine();
 var esInPar =
@@ -29,22 +29,22 @@ var esInPar =
     where (inpar % 2) != 0
     select inpar;
 
-foreach(int i in esInPar)
+foreach (int i in esInPar)
     Console.WriteLine(i);
 
 #endregion
 
 #region Segunda forma 
-Console.WriteLine("Mostrando de la segunda forma");
+Console.WriteLine("Mostrando con Method syntax");
 var divisibleEntreSiete = listEnteros.Where(x => x % 7 == 0);
 
 var esParSiete = divisibleEntre7.Where(x => x % 2 == 0);
 
 var esImparSiete = divisibleEntre7.Where(x => x % 2 != 0);
 
-foreach(int i in divisibleEntreSiete) Console.WriteLine(i);
+foreach (int i in divisibleEntreSiete) Console.WriteLine(i);
 Console.WriteLine();
 foreach (int i in esParSiete) Console.WriteLine(i);
 Console.WriteLine();
-foreach(int i in esImparSiete) Console.WriteLine(i);
+foreach (int i in esImparSiete) Console.WriteLine(i);
 #endregion
