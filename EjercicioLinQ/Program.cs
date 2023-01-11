b55019dda5aa2e7bfc5bea0e380791d1b9f58646
DESKTOP-QMGOQCG\CSI21-Amorlez <csi21-amorlez@colegioaltair.net>
2023-01-11 14:35:02 +0100

    Optimizacion

--- EjercicioLinQ/Program.cs
+++ EjercicioLinQ/Program.cs
@@ -1,51 +1,44 @@
 ﻿using System;
 
-
 List<int> listEnteros = new List<int>();
 
 for (int i = 0; i < 100; i++)
+    if(i%7 ==0)
     listEnteros.Add(i);
 
 
 #region Query syntax
-Console.WriteLine("Mostrando con Query syntax");
-var divisibleEntre7 =
-    from entre in listEnteros
-    where (entre % 7) == 0
-    select entre;
-
+Console.WriteLine("\n Mostrando con Query syntax");
 
 Console.WriteLine();
 var esPar =
-    from par in divisibleEntre7
+    from par in listEnteros
     where (par % 2) == 0
     select par;
 
-foreach (int i in esPar)
-    Console.WriteLine(i);
-
 var esInPar =
-    from inpar in divisibleEntre7
+    from inpar in listEnteros
     where (inpar % 2) != 0
     select inpar;
 
-foreach (int i in esPar) Console.WriteLine(i);
+foreach (int i in listEnteros) Console.WriteLine(i);
 Console.WriteLine();
-foreach (int i in divisibleEntre7) Console.WriteLine(i);
+foreach (int i in esPar) Console.WriteLine(i);
 Console.WriteLine();
 foreach (int i in esInPar) Console.WriteLine(i);
 
 #endregion
 
 #region Method syntax
-Console.WriteLine("Mostrando con Method syntax");
-var divisibleEntreSiete = listEnteros.Where(x => x % 7 == 0);
+Console.WriteLine("\n Mostrando con Method syntax");
 
-var esParSiete = divisibleEntre7.Where(x => x % 2 == 0);
 
-var esImparSiete = divisibleEntre7.Where(x => x % 2 != 0);
+var esParSiete = listEnteros.Where(x => x % 2 == 0);
 
-foreach (int i in divisibleEntreSiete) Console.WriteLine(i);
+var esImparSiete = listEnteros.Where(x => x % 2 != 0);
+
+
+foreach(int i in listEnteros)Console.WriteLine(i);
 Console.WriteLine();
 foreach (int i in esParSiete) Console.WriteLine(i);
 Console.WriteLine();
